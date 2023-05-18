9d5917793f7054463120a8427e36ad453514440f
nil-5da3 <n.petruccioli@gmail.com>
2023-05-18 23:42:36 +0200

    update accounts prefix with vesta

--- cmd/vestad/cmd/config.go
+++ cmd/vestad/cmd/config.go
@@ -2,21 +2,21 @@ package cmd
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
-
-	"vesta/app"
 )
+
+const prefix = "vesta"
 
 func initSDKConfig() {
 	// Set prefixes
-	accountPubKeyPrefix := app.AccountAddressPrefix + "pub"
-	validatorAddressPrefix := app.AccountAddressPrefix + "valoper"
-	validatorPubKeyPrefix := app.AccountAddressPrefix + "valoperpub"
-	consNodeAddressPrefix := app.AccountAddressPrefix + "valcons"
-	consNodePubKeyPrefix := app.AccountAddressPrefix + "valconspub"
+	accountPubKeyPrefix := prefix + "pub"
+	validatorAddressPrefix := prefix + "valoper"
+	validatorPubKeyPrefix := prefix + "valoperpub"
+	consNodeAddressPrefix := prefix + "valcons"
+	consNodePubKeyPrefix := prefix + "valconspub"
 
 	// Set and seal config
 	config := sdk.GetConfig()
-	config.SetBech32PrefixForAccount(app.AccountAddressPrefix, accountPubKeyPrefix)
+	config.SetBech32PrefixForAccount(prefix, accountPubKeyPrefix)
 	config.SetBech32PrefixForValidator(validatorAddressPrefix, validatorPubKeyPrefix)
 	config.SetBech32PrefixForConsensusNode(consNodeAddressPrefix, consNodePubKeyPrefix)
 	config.Seal()
